f01988ca7cc13c501df74d0a0c5f2a63647d2e7a
giovannabadaro <giovannaalves@visaojr.com.br>
2018-12-12 15:42:05 -0200

    primeiro commit

---
+++ docs/mydb.sql
@@ -0,0 +1,52 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
+  `idusuario` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `idade` DATE NOT NULL,
+  `peso` DOUBLE NOT NULL,
+  `altura` DOUBLE NOT NULL,
+  `sexo` CHAR(1) NOT NULL,
+  PRIMARY KEY (`idusuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`registro`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`registro` (
+  `idregistro` INT NOT NULL,
+  `kcal` DOUBLE NULL,
+  `agua` DOUBLE NULL,
+  `atvfisica` DOUBLE NULL,
+  `usuario_idusuario` INT NOT NULL,
+  PRIMARY KEY (`idregistro`),
+  INDEX `fk_registro_usuario_idx` (`usuario_idusuario` ASC) registro,
+  CONSTRAINT `fk_registro_usuario`
+    FOREIGN KEY (`usuario_idusuario`)
+    REFERENCES `mydb`.`usuario` (`idusuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
